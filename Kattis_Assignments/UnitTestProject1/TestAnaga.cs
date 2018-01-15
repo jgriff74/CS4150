7468570ec571149a31171ea0951fdaef203b2aa9
River <griffinjade8@gmail.com>
2018-01-15 15:20:45 -0700

    Created 2 projects

---
+++ Kattis_Assignments/UnitTestProject1/TestAnaga.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mr_Anaga;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class TestAnaga
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            NotAnagram s = new NotAnagram();
+        }
+    }
+}
